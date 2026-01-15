4f90db64c5ea9b5fd4411dd4df5dfa96edaaed85
Pratiksha Kate <katepratiksha09@gmail.com>
2026-01-15 15:12:47 +0530

    15jan index.css changes

--- src/BioGasComponentes/Index19.jsx
+++ src/BioGasComponentes/Index19.jsx
@@ -62,7 +62,7 @@ const ProposalTable = () => {
     <section className="proposal-wrapper">
       {/* HEADER */}
       <div className="proposal-header" ref={bannerRef}>
-        <div className={`arrow-index ${visible ? "animate-arrow" : ""}`}>
+        <div className={`arrow-indexo ${visible ? "animate-arrow" : ""}`}>
           <svg className="arrow-svg" viewBox="0 0 120 180">
             <polygon
               points="0,0 84,0 120,90 84,180 0,180 36,90"
